f4ab53caac878a11eed090ba0f51b72e055c5916
paul-boerding <bababax@hotmail.de>
2021-09-11 11:16:26 +0200

    wrote a first test for multiply

--- code/src/01-multiply/multiply.spec.js
+++ code/src/01-multiply/multiply.spec.js
@@ -1,5 +1,10 @@
 const { multiply } = require('./multiply');
 
 test('multiply 3 * 5 is equal to 15', () => {
-  expect(multiply(3,5)).toBe(15);
+  const inputA = 3;
+  const inputB = 5;
+  const expectedOutput = 15;
+  const actualOutput = multiply(inputA, inputB);
+
+  expect(actualOutput).toBe(expectedOutput);
 });
